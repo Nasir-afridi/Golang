2fc6b40ac46849d0b3dc00cf81b37002c13257ee
Nasir-afridi <nasirwc05@gmail.com>
2025-07-28 21:12:54 +0500

    Added the variables topic

---
+++ 3_variables/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Declare a variables
+	var name string = "Nasir"
+
+	// infer
+	var adress = "Pakistan"
+
+	var age int = 23
+	fmt.Println(name)
+	fmt.Println(adress)
+	fmt.Println(age)
+
+	//shorthand method
+
+	city := "wah cantt"
+	fmt.Println(city)
+
+}
