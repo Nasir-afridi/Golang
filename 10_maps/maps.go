c97df69556a6d288c58a0eb7920e677c92d64288
Nasir-afridi <nasirwc05@gmail.com>
2025-08-07 14:24:35 +0500

    creating the map, adding the elements to it, checking the length, delete the element from the map.

---
+++ 10_maps/maps.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[string]string) // initialized the map.
+
+	// Adding the elements
+	m["name"] = "golang"
+	m["Area"] = "backend"
+	m["phone"] = "0000"
+
+	// get an element
+	fmt.Println(m["name"], m["Area"], m["phone"])
+	fmt.Println(len(m)) // checking the length
+
+	delete(m, "phone") // delete the element from the map.
+	fmt.Println(m)
+}
