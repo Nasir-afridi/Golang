65ed3e1d8259d7f99a3acc770e146d88a3c961fe
Nasir-afridi <nasirwc05@gmail.com>
2025-08-07 16:36:31 +0500

    check the element of map is exists or not and two maps are equal to each other or not

--- 10_maps/maps.go
+++ 10_maps/maps.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"maps"
+)
 
 func main() {
 	m := make(map[string]string) // initialized the map.
@@ -19,4 +22,22 @@ func main() {
 
 	clear(m) // clear the map
 	fmt.Println(m)
+
+	// fixed length kk liye yane static map.
+	a := map[string]int{"phones": 4, "price": 50}
+	fmt.Println(a)
+
+	// checking the element is in the map or not.
+
+	_, hi := a["phones"] // underscore syyy ye zahir hota hai kkk ham phones ki value ko ignore krrhy hain .
+	if hi {
+		fmt.Println("hi all ok ")
+	} else {
+		fmt.Println("Not ok")
+	}
+
+	// checking that the maps are equal or not
+	a1 := map[string]int{"phones": 4, "price": 50}
+	a2 := map[string]int{"phones": 4, "price": 50}
+	fmt.Println(maps.Equal(a1, a2))
 }
